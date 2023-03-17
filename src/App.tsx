477e7d1301c29fe1b0d2f90a505bcada6aaf7f21
David-Koshkadze <rootUS3rr@pm.me>
2023-03-17 16:50:27 +0400

    ch1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+
+// const URL = "http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch(`${URL}/user/1/10`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <div>
-      <h1 className="text-2xl font-sans bg-green-300 text-center">Hello World</h1>
+      <h1 className="text-2xl font-sans bg-green-300 text-center">
+        Hello World
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
